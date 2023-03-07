2410521d3a8d99d93a25278138b71f2ade47b24f
mttaskin <0mttaskin1@gmail.com>
2023-03-07 21:20:35 +0300

    v3

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { GlobalStyles } from './styles/Global.styles';
-import Navbar from "../components/navbar"
-import Footer from "../components/footer"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GlobalStyles } from "../styles/Global.styles";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+import PrivateRouter from "./PrivateRouter";
+import Detail from "../pages/detail/Detail";
+import About from "../pages/about/About";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-    <Navbar/>
-    <GlobalStyles/>
-    <Routes>
-<Route path='/' element={<Home/>}/>
-<Route path='/login' element={<Login/>}/>
-
-
-    </Routes>
-    <Footer/>
+      <Navbar />
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<PrivateRouter />}>
+          <Route path="" element={<About />} />
+        </Route>
+        <Route path="/detail" element={<PrivateRouter />}>
+          <Route path="" element={<Detail />} />
+        </Route>
+      </Routes>
+      <Footer />
     </BrowserRouter>
+  );
+};
 
-  )
-}
-
-export default AppRouter
+export default AppRouter;
