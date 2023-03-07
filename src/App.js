4a8869aaf7fd32ac09d9d2cb621837c95bea19c5
mttaskin <0mttaskin1@gmail.com>
2023-03-07 20:46:32 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { ThemeProvider } from 'styled-components';
 import AppRouter from './router/AppRouter';
+import { GlobalStyles } from './styles/Global.styles';
+import { theme } from './styles/theme';
+
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
       <AppRouter/>
-    </div>
+    </ThemeProvider>
   );
 }
 
