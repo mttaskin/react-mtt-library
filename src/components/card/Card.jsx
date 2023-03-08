9acc389a8f0f793b4db6e6bb68a7351f83bc0ef3
mttaskin <0mttaskin1@gmail.com>
2023-03-08 22:29:16 +0300

    v14

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { CardButton, CardContainer, CardHeader, CardImg } from './Card.style'
 import defaultImg from "../../assets/book.jpg"
-import { Navigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Card = ({item}) => {
-  const navigate = Navigate()
+  const navigate = useNavigate()
+
   console.log(item)
+
   return (
     <CardContainer>
       <CardHeader>{item.volumeInfo.title}</CardHeader>
-      <CardImg scr={item.volumeInfo.imageLinks.smallThumbnail || defaultImg} />
-      <CardButton onClick={()=>navigate(`/detail/${item.id}`,{state:item})}>View More</CardButton>
+      <CardImg src={item.volumeInfo.imageLinks?.smallThumbnail || defaultImg} />
+      <CardButton onClick={()=> navigate(`/detail/${item.id}`,{state:item})}>View More</CardButton>
     </CardContainer>
   );
 }
