5bb09c08a8bd83bcc3a93d5c432309ed7b12a4af
mttaskin <0mttaskin1@gmail.com>
2023-03-09 00:35:50 +0300

    v16

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import { AboutContainer, IconContainer, InfoContainer, ProfileImg } from './About.style'
-import defaultImg from "../../assets/book.jpg";
+import profile from "../../assets/about.png"
+import Icon from '../../components/icon/Icon';
+import { iconData } from '../../helper/iconData';
 
 const About = () => {
   return (
-   <AboutContainer>
-    <ProfileImg>
+    <AboutContainer>
+      <ProfileImg src={profile} alt="anthony" />
       <InfoContainer>
-
+        <h2>Hi, I'm Anthony</h2>
+        <h3>I’m currently learning Full-Stack Development Languages.</h3>
+        <h4>
+          I've already known JS, ReactJS, NextJs, ReactNative, Django, NodeJS,
+          MongoDB,SQL, Python, AWS Services.
+        </h4>
+        <h4>💬 You can ask me anything you want to know</h4>
       </InfoContainer>
       <IconContainer>
-
+        {iconData.map(item=> <a key={item.icon} href={item.href}>
+          <Icon icon={item.icon} path={item.path} hoverColor={item.color} />
+        </a>)}
       </IconContainer>
-    </ProfileImg>
-   </AboutContainer>
-  )
+    </AboutContainer>
+  );
 }
 
 export default About
