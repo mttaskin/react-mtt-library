3419057931cd11b461207955107c6dde24fc1214
mttaskin <0mttaskin1@gmail.com>
2023-03-08 22:42:02 +0300

    v15

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useLocation, useParams } from 'react-router-dom'
+import { Description, DetailContainer, DetailImg, DetailTitle, InfoPart } from "./Detail.style";
+import defaultImg from "../../assets/book.jpg"
 
 const Detail = () => {
   const {state} = useLocation();
@@ -7,7 +9,20 @@ const Detail = () => {
   console.log(id)
   console.log(state)
   return (
-    <div>Detail</div>
+    <DetailContainer>
+      <DetailTitle>
+        {state.volumeInfo.title}
+      </DetailTitle>
+      <DetailImg>
+        <img src={state.volumeInfo.imageLinks?.smallThumbnail || defaultImg} alt={state.volumeInfo.title} />
+      </DetailImg>
+      <Description>
+        {state.volumeInfo.description}
+      </Description>
+      <InfoPart>
+        
+      </InfoPart>
+    </DetailContainer>
   )
 }
 
